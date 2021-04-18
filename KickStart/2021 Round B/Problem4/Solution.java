fce0e357a1c5a439b1bb1b83db2023458bd164d0
DanielMelero <daniel.melero.001@gmail.com>
2021-04-19 00:06:39 +0200

    Preparation for kickStart round B

---
+++ KickStart/2021 Round B/Problem4/Solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * @author Daniel Melero
+ */
+public class Solution {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+		int cases = in.nextInt();
+		Solution solution = new Solution();
+		for (int i = 1; i <= cases; ++i) {
+			solution.solve(i, in);
+		}
+	}
+
+	public void solve(int c, Scanner in) {
+		int n = in.nextInt(); // Scanner has functions to read ints, longs, strings, chars, etc.
+		
+		/**
+		 * Solve Problem Here.
+		 */
+
+		this.print(c, n);
+	}
+
+	private void print(int c, String res) {
+		System.out.println("Case #" + c + ": " + res);
+	}
+
+	private void print(int c, int res) {
+		this.print(c, "" + res);
+	}
+}
